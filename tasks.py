57f7ac3424c34bffa1e30f56e87e8fa2d8743a9d
Nguyen2442 <faker2442@gmail.com>
2023-03-27 09:51:40 +0700

    done

--- tasks.py
+++ tasks.py
@@ -12,3 +12,22 @@ def run_hello():
 
 
 run_hello()
+
+
+# function use to download excel
+def handle_excel(filename):
+    import xlrd
+    book = xlrd.open_workbook(filename)
+    sheet = book.sheet_by_index(0)
+    rows = sheet.nrows
+    cols = sheet.ncols
+    data = []
+    for i in range(1, rows):
+        row = []
+        for j in range(1, cols):
+            row.append(sheet.cell_value(i, j))
+        data.append(row)
+    return data
+
+# generate a function to download file excel
+# return a binary file
